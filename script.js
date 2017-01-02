19a727f3ffe003881216425e8615986a177d7653
James Cool <jeffreysbrother@gmail.com>
2017-01-01 16:31:41 -0800

    add json instead of XML

--- script.js
+++ script.js
@@ -7,19 +7,16 @@ if (window.XMLHttpRequest) {
 }
 
 // an asynchronous request
-request.open('GET', 'data.xml');
+request.open('GET', 'data.json');
 request.onreadystatechange = function() {
   if ((request.readyState === 4) && (request.status === 200)) {
-    console.log(request.responseXML.getElementsByTagName('name')[1].firstChild.nodeValue);
-
-    var items = request.responseXML.getElementsByTagName('name');
+    var items = JSON.parse(request.responseText);
     var output = '<ul>';
-    for (var i = 0; i < items.length; i ++) {
-      output += '<li>' +
-      items[i].firstChild.nodeValue + '</li>';
+    for (var key in items) {
+      output += '<li>' + items[key].name + '</li>';
     }
     output += '</ul>';
-    document.getElementById('update').innerHTML = output;
+    document.getElementById('update').innerHTML =  output;
   }
 }
 request.send();
