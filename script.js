40cf6512a431fe4eabcc396f1e6ca42466c422c9
James Cool <jeffreysbrother@gmail.com>
2017-01-01 16:26:48 -0800

    pull data from XML to ul in HTML

--- script.js
+++ script.js
@@ -10,7 +10,16 @@ if (window.XMLHttpRequest) {
 request.open('GET', 'data.xml');
 request.onreadystatechange = function() {
   if ((request.readyState === 4) && (request.status === 200)) {
-    console.log(request.responseXML);
+    console.log(request.responseXML.getElementsByTagName('name')[1].firstChild.nodeValue);
+
+    var items = request.responseXML.getElementsByTagName('name');
+    var output = '<ul>';
+    for (var i = 0; i < items.length; i ++) {
+      output += '<li>' +
+      items[i].firstChild.nodeValue + '</li>';
+    }
+    output += '</ul>';
+    document.getElementById('update').innerHTML = output;
   }
 }
 request.send();
