7348a36aae64b2ffd83c29c5e8c1a26e5ab989ac
James Cool <jeffreysbrother@gmail.com>
2017-03-20 21:50:51 -0700

    app working

--- flickr/js/flickr.js
+++ flickr/js/flickr.js
@@ -1,6 +1,23 @@
 $("button").click(function() {
   $("button").removeClass("selected");
   $(this).addClass("selected");
+  var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+  var animal = $(this).text();
+  var flickrOptions = {
+    tags: animal,
+    format: "json"
+  };
+  var displayPhotos = function(data) {
+    var photoHTML = '<ul>';
+    $.each(data.items, function(i, photo) {
+      photoHTML += '<li class="grid-25 tablet-grid-50">';
+      photoHTML += '<a href="' + photo.link + '" class="image">';
+      photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+    });
+    photoHTML += '</ul>';
+    $('#photos').html(photoHTML);
+  }
+  $.getJSON(flickrAPI, flickrOptions, displayPhotos);
 });
 
 
