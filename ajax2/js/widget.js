979da4efd2f040ec1dc144fac0cb75fcfe144a55
James Cool <jeffreysbrother@gmail.com>
2017-03-20 19:23:48 -0700

    finish creating AJAX request for meeting room status

--- ajax2/js/widget.js
+++ ajax2/js/widget.js
@@ -1,3 +1,4 @@
+// AN AJAX REQUEST for employee status
 // create XMLHttpRequest object
 var xhr = new XMLHttpRequest();
 // create callback
@@ -22,3 +23,33 @@ xhr.onreadystatechange = function() {
 xhr.open('GET', 'data/employees.json');
 // send request
 xhr.send();
+
+
+
+
+
+// AN AJAX REQUEST for available rooms
+// create XMLHttpRequest object
+var ajax = new XMLHttpRequest();
+// create callback
+ajax.onreadystatechange = function() {
+  if (ajax.readyState === 4) {
+    var rooms = JSON.parse(ajax.responseText);
+    var roomStatusHTML = '<ul class="rooms">';
+    for (var i=0; i<rooms.length; i++) {
+      if (rooms[i].available === true) {
+        roomStatusHTML += '<li class="empty">';
+      } else {
+        roomStatusHTML += '<li class="full">';
+      }
+      roomStatusHTML += rooms[i].room;
+      roomStatusHTML += '</li>';
+    }
+    roomStatusHTML += '</ul>';
+    document.getElementById('roomList').innerHTML = roomStatusHTML;
+  }
+};
+// open a request
+ajax.open('GET', 'data/rooms.json');
+// send request
+ajax.send();
