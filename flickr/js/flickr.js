85c9ec8c5d094f051faab7a57b5f740d9be0a7b6
James Cool <jeffreysbrother@gmail.com>
2017-03-20 20:25:34 -0700

    use jquery to add click state

---
+++ flickr/js/flickr.js
@@ -0,0 +1,7 @@
+$("button").click(function() {
+  $("button").removeClass("selected");
+  $(this).addClass("selected");
+});
+
+
+// https://api.flickr.com/services/feeds/photos_public.gne?format=json
