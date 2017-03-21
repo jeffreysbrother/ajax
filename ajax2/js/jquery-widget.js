4d90a00fd83b6005a89e48b7f6e6fe7681464b19
James Cool <jeffreysbrother@gmail.com>
2017-03-20 19:55:22 -0700

    convert js to jquery

--- ajax2/js/jquery-widget.js
+++ ajax2/js/jquery-widget.js
@@ -1,55 +1,40 @@
-// AN AJAX REQUEST for employee status
-// create XMLHttpRequest object
-var xhr = new XMLHttpRequest();
-// create callback
-xhr.onreadystatechange = function() {
-  if (xhr.readyState === 4) {
-    var employees = JSON.parse(xhr.responseText);
-    var statusHTML = '<ul class="bulleted">';
-    for (var i=0; i<employees.length; i++) {
-      if (employees[i].inoffice === true) {
-        statusHTML += '<li class="in">';
-      } else {
-        statusHTML += '<li class="out">';
-      }
-      statusHTML += employees[i].name;
-      statusHTML += '</li>';
+// A jquery AJAX REQUEST for employee status
+var url = "data/employees.json"
+
+$.getJSON(url, function(response) {
+var statusHTML = '<ul class="bulleted">';
+  $.each(response, function(index, employee) {
+    if (employee.inoffice === true) {
+      statusHTML += '<li class="in">';
+    } else {
+      statusHTML += '<li class="out">';
     }
-    statusHTML += '</ul>';
-    document.getElementById('employeeList').innerHTML = statusHTML;
-  }
-};
-// open a request
-xhr.open('GET', 'data/employees.json');
-// send request
-xhr.send();
+    statusHTML += employee.name + '</li>';
+  });
+  statusHTML += '</ul>';
+  $('#employeeList').html(statusHTML);
+});
 
 
 
 
 
-// AN AJAX REQUEST for available rooms
-// create XMLHttpRequest object
-var ajax = new XMLHttpRequest();
-// create callback
-ajax.onreadystatechange = function() {
-  if (ajax.readyState === 4) {
-    var rooms = JSON.parse(ajax.responseText);
-    var roomStatusHTML = '<ul class="rooms">';
-    for (var i=0; i<rooms.length; i++) {
-      if (rooms[i].available === true) {
-        roomStatusHTML += '<li class="empty">';
-      } else {
-        roomStatusHTML += '<li class="full">';
-      }
-      roomStatusHTML += rooms[i].room;
-      roomStatusHTML += '</li>';
+
+
+
+// A jquery AJAX REQUEST for available rooms
+var roomurl = "data/rooms.json"
+
+$.getJSON(roomurl, function(response) {
+var roomStatusHTML = '<ul class="rooms">';
+  $.each(response, function(index, rooms) {
+    if (rooms.available === true) {
+      roomStatusHTML += '<li class="empty">';
+    } else {
+      roomStatusHTML += '<li class="full">';
     }
-    roomStatusHTML += '</ul>';
-    document.getElementById('roomList').innerHTML = roomStatusHTML;
-  }
-};
-// open a request
-ajax.open('GET', 'data/rooms.json');
-// send request
-ajax.send();
+    roomStatusHTML += rooms.room + '</li>';
+  });
+  roomStatusHTML += '</ul>';
+  $('#roomList').html(roomStatusHTML);
+});
